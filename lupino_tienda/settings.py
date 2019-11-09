a242d417641366dcbc19c371168e39ff04859c68
mo.sepulvedar <mo.sepulvedar@profesor.duoc.cl>
2019-11-09 15:21:22 -0300

    prueba conflicto

--- lupino_tienda/settings.py
+++ lupino_tienda/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-#comentario del profe
+#comentario del profe otro comentario mas de prueba
+#comentando...
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
